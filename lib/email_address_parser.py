a001b3e70c59d946201464f867f1dca84a84b4ed
Hingaz <hhinga04@gmail.com>
2023-12-08 22:15:12 +0300

    Completed Lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+
+        addresses = re.split(r'[,\s]+', self.email_addresses)
+
+        valid_addresses = list(
+            filter(lambda address: address != '', addresses))
+
+        return valid_addresses
